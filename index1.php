583b464f652d93edddf4c9f09b2e03726d577190
Eduardo Antonio Neiss <100316865+oManoNeiss@users.noreply.github.com>
2022-08-30 09:18:22 -0300

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="palavras-chave-aqui">
+    <script src="https://kit.fontawesome.com/8dac680685.js" crossorigin="anonymous"></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@300&display=swap" rel="stylesheet">
+    <title>TAI</title>
+</head>
+
+<body>
+    <header>
+        <div class="center">
+            <div class="logo left">
+                <!--Ajustando a responsividade do site-->
+                <nav class="desktop right">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Serviços</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </nav>
+                <nav class="mobile right">
+                    <ul>
+                        <li><a href="">Home</a></li>
+                        <li><a href="">Sobre</a></li>
+                        <li><a href="">Serviços</a></li>
+                        <li><a href="">Contato</a></li>
+                    </ul>
+                </nav>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </header>
+    <!--Banner-->
+    <section class="banner-principal">
+        <div class="overlay"></div>
+        <div class="center">
+            <form>
+                <h2>Qual seu melhor e-mail?</h2>
+                <input type="email" name="email" required>
+                <input type="submit" name="acao" value="Cadastrar">
+            </form>
+        </div>
+
+    </section>
+    <section class="desc-autor">
+        <div class="center">
+            <div class="w50 left">
+                <h2>Neiss</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis architecto maiores velit, obcaecati hic
+                    exercitationem ea possimus accusantium facilis asperiores? Quod sequi maxime ea illum tenetur ipsum
+                    corrupti nostrum autem.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis aperiam qui omnis voluptates nam
+                    eveniet nihil et illum odio vel, non culpa quaerat commodi labore vitae cumque est corporis consequuntur?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam error magni delectus tempore pariatur
+                    assumenda? Quaerat iste, beatae debitis obcaecati quae tempore recusandae perspiciatis odio, nam voluptate
+                    temporibus! Vitae, minima.
+                </p>
+            </div>
+            <div class="w50 left">
+                <img src="Imagens/freddie.jpg">
+            </div>
+        </div>
+        <div class="clear"></div>
+    </section>
+    <section class ="especialidades">
+        <div class="center">
+            <h2 class ="title">Especialidades</h2>
+
+            <div class="w33 left box-especialidade">
+                <h3><i class="fa-brands fa-css3"></i></h3>
+                <h3>CSS3</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo magnam rerum animi ut. Iure, ipsum eius in 
+                    facere nobis culpa corporis voluptas animi doloremque ad dolores fuga earum vitae nemo! Lorem ipsum dolor 
+                    sit amet consectetur adipisicing elit. Sed inventore accusamus exercitationem labore dolorum aperiam magni 
+                    cum doloremque iste ducimus? Tempora officiis voluptates animi corporis quod debitis explicabo rem mollitia.
+                </p>
+            </div>
+            <div class="w33 left box-especialidade">
+                <h3><i class="fa-brands fa-html5"></i></h3>
+                <h3>HTML5</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo magnam rerum animi ut. Iure, ipsum eius in 
+                    facere nobis culpa corporis voluptas animi doloremque ad dolores fuga earum vitae nemo! Lorem ipsum dolor 
+                    sit amet consectetur adipisicing elit. Sed inventore accusamus exercitationem labore dolorum aperiam magni 
+                    cum doloremque iste ducimus? Tempora officiis voluptates animi corporis quod debitis explicabo rem mollitia.
+                </p>
+            </div>
+            <div class="w33 left box-especialidade">
+                <h3><i class="fa-brands fa-js"></i></h3>
+                <h3>JavaScript</h3>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nemo magnam rerum animi ut. Iure, ipsum eius in 
+                    facere nobis culpa corporis voluptas animi doloremque ad dolores fuga earum vitae nemo! Lorem ipsum dolor 
+                    sit amet consectetur adipisicing elit. Sed inventore accusamus exercitationem labore dolorum aperiam magni 
+                    cum doloremque iste ducimus? Tempora officiis voluptates animi corporis quod debitis explicabo rem mollitia.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="extras">
+        <div class="center">
+            <div class="w50 left">
+                <h2 class="title">Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class="depoimento-desc">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum dolorem, dignissimos adipisci 
+                        inventore reiciendis expedita totam delectus quibusdam porro, ea in cupiditate fugiat rerum 
+                        perferendis, voluptas explicabo reprehenderit molestiae commodi?Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Odit quaerat illum tempore magnam vero! Atque rerum nam porro 
+                        id iste molestiae placeat odit! Illum, iusto inventore perspiciatis sapiente deserunt 
+                        consectetur!
+                    </p>
+                    <p class="nome-autor">~Freddie Mercury</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-desc">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum dolorem, dignissimos adipisci 
+                        inventore reiciendis expedita totam delectus quibusdam porro, ea in cupiditate fugiat rerum 
+                        perferendis, voluptas explicabo reprehenderit molestiae commodi?Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Odit quaerat illum tempore magnam vero! Atque rerum nam porro 
+                        id iste molestiae placeat odit! Illum, iusto inventore perspiciatis sapiente deserunt 
+                        consectetur!
+                    </p>
+                    <p class="nome-autor">~Velma</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class="depoimento-desc">
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum dolorem, dignissimos adipisci 
+                        inventore reiciendis expedita totam delectus quibusdam porro, ea in cupiditate fugiat rerum 
+                        perferendis, voluptas explicabo reprehenderit molestiae commodi?Lorem ipsum dolor sit amet 
+                        consectetur adipisicing elit. Odit quaerat illum tempore magnam vero! Atque rerum nam porro 
+                        id iste molestiae placeat odit! Illum, iusto inventore perspiciatis sapiente deserunt 
+                        consectetur!
+                    </p>
+                    <p class="nome-autor">~Salsicha</p>
+                </div>
+                
+            </div>
+            <div class="w50 left">
+                <h2 class="title">Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur, iusto maxime veniam 
+                            non minus, nisi error eligendi alias nesciunt fuga beatae laudantium recusandae aperiam, 
+                            repellendus illo obcaecati totam nam eveniet. Lorem ipsum dolor sit amet consectetur 
+                            adipisicing elit. Aperiam vel perspiciatis nisi, saepe recusandae, fugiat delectus adipisci,
+                             voluptatum dolor modi natus repudiandae doloremque! Voluptatem voluptate reprehenderit, 
+                             ullam laboriosam magni optio.
+                        </li>
+                        <br>
+                        <li>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur, iusto maxime veniam 
+                            non minus, nisi error eligendi alias nesciunt fuga beatae laudantium recusandae aperiam, 
+                            repellendus illo obcaecati totam nam eveniet. Lorem ipsum dolor sit amet consectetur 
+                            adipisicing elit. Aperiam vel perspiciatis nisi, saepe recusandae, fugiat delectus adipisci,
+                             voluptatum dolor modi natus repudiandae doloremque! Voluptatem voluptate reprehenderit, 
+                             ullam laboriosam magni optio.
+                        </li>
+                        <br>
+                        <li>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur, iusto maxime veniam 
+                            non minus, nisi error eligendi alias nesciunt fuga beatae laudantium recusandae aperiam, 
+                            repellendus illo obcaecati totam nam eveniet. Lorem ipsum dolor sit amet consectetur 
+                            adipisicing elit. Aperiam vel perspiciatis nisi, saepe recusandae, fugiat delectus adipisci,
+                             voluptatum dolor modi natus repudiandae doloremque! Voluptatem voluptate reprehenderit, 
+                             ullam laboriosam magni optio.
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer>
+        <div class="center">
+            <p>Todos os direitos reservados</p>
+        </div>
+    </footer>
+</body>
+</html>
